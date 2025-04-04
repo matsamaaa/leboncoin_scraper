2785dee46265d7755cf33b80b665f0de897dd4a1
mat_sama <mateosejourne@gmail.com>
2025-04-04 17:52:22 +0200

    feat(log)

---
+++ src/utils/logs.js
@@ -0,0 +1,22 @@
+import chalk from 'chalk';
+import dateformat from 'dateformat';
+
+class Log {
+    static Date() {
+        return dateformat(new Date(), 'dd-mm-yyyy HH:MM:ss');
+    }
+
+    static Success(message) {
+        console.log(`${chalk.white.bgGreen.bold(`SUCCESS`)} [${Log.Date()}] » ${message}`);
+    }
+
+    static Error(message) {
+        console.log(`${chalk.white.bgRed.bold(`ERROR`)} [${Log.Date()}] » ${message}`);
+    }
+
+    static Info(message) {
+        console.log(`${chalk.white.bgBlue.bold(`INFO`)} [${Log.Date()}] » ${message}`);
+    }
+}
+
+export default Log;
